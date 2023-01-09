fecdd5018d7d3eb90558855d16ff3d83cb33078d
Jordan Murray <Jordan.Murray@ibm.com>
2023-01-09 15:57:28 -0500

    created basic quantum circuit to generate a random number 1-9

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from qiskit import *
 from matplotlib import *
 
@@ -9,10 +9,18 @@ def hello():
     return "<p>Hello, world</p>"
 
 @app.route("/qiskit")
-def qiskit():
-    qr = QuantumRegister(2)
-    cr = ClassicalRegister(2)
+def my_qiskit():
+    qr = QuantumRegister(4)
+    cr = ClassicalRegister(4)
     circuit = QuantumCircuit(qr, cr)
     #%matplotlib inline
-    circuit.draw()
-    return "<p>test</p>"
+    circuit.h(qr[0])
+    circuit.h(qr[1])
+    circuit.h(qr[2]) 
+    circuit.h(qr[3])
+    circuit.measure(qr,cr)
+    simulator = Aer.get_backend('qasm_simulator')
+    result = execute(circuit, backend=simulator, shots=1).result()
+    number = result.get_counts(circuit)
+    #print(result.get_counts(circuit))
+    return render_template("index.html", number=number)
