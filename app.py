9c7e8f3d4c936e93db003e7ecf7fbf8d2b43ae88
Turbanator911 <68335929+Turbanator911@users.noreply.github.com>
2023-01-09 15:03:52 -0500

    Started qiskit/flask code

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from qiskit import *
+from matplotlib import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "<p>Hello, world</p>"
+
+@app.route("/qiskit")
+def qiskit():
+    qr = QuantumRegister(2)
+    cr = ClassicalRegister(2)
+    circuit = QuantumCircuit(qr, cr)
+    #%matplotlib inline
+    circuit.draw()
+    return "<p>test</p>"
